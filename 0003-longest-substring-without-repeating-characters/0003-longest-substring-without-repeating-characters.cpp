5e31b5e4b5bc01c2cead374ffe2c2c1088d32011
Tushar Mishra <118966111+tusharmishra069@users.noreply.github.com>
2024-09-24 21:13:40 +0530

    Time: 22 ms (32.34%), Space: 14.7 MB (18.13%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -6,12 +6,12 @@ public:
     
     while (right < s.length()) {
         if (check.find(s[right]) == check.end()) {
-            // If the character is not in the set, add it and move the right pointer
+            
             check.insert(s[right]);
             maxLength = max(maxLength, right - left + 1);
             right++;
         } else {
-            // If the character is already in the set, remove the character at 'left' and move 'left'
+            
             check.erase(s[left]);
             left++;
         }

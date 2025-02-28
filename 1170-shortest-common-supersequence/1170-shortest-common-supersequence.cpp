bd4d9d56e79f58ad502c9982bea96a1428dc48b4
Tushar Mishra <118966111+tusharmishra069@users.noreply.github.com>
2025-02-28 23:45:36 +0530

    Time: 22 ms (91.23%), Space: 26.7 MB (70.92%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
@@ -4,7 +4,6 @@ public:
         int m = str1.size();
         int n = str2.size();
 
-        // Step 1: Compute LCS length using DP
         vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
         for (int i = 1; i <= m; ++i) {
             for (int j = 1; j <= n; ++j) {
@@ -15,12 +14,11 @@ public:
             }
         }
 
-        // Step 2: Reconstruct the shortest common supersequence (SCS)
         string ans;
         int i = m, j = n;
         
         while (i > 0 && j > 0) {
-            if (str1[i - 1] == str2[j - 1]) { // Part of LCS
+            if (str1[i - 1] == str2[j - 1]) { 
                 ans += str1[i - 1];
                 --i, --j;
             } else if (dp[i - 1][j] >= dp[i][j - 1]) {
@@ -32,11 +30,10 @@ public:
             }
         }
 
-        // Add remaining characters from str1 or str2
         while (i > 0) ans += str1[--i];
         while (j > 0) ans += str2[--j];
 
-        reverse(ans.begin(), ans.end());  // Reverse to get the correct order
+        reverse(ans.begin(), ans.end());  
         return ans;
     }
 };

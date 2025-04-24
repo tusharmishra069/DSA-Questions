dd9318bcaa6fec82cea53dc39fe8d352a000d101
Tushar Mishra <118966111+tusharmishra069@users.noreply.github.com>
2025-04-24 21:38:45 +0530

    Time: 13 ms (86.21%), Space: 50.5 MB (25.3%) - LeetHub

---
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+ public:
+  int countCompleteSubarrays(vector<int>& nums) {
+    constexpr int kMax = 2000;
+    const int totalDistinct =
+        unordered_set<int>(nums.begin(), nums.end()).size();
+    int ans = 0;
+    int distinct = 0;
+    vector<int> count(kMax + 1);
+
+    int l = 0;
+    for (const int num : nums) {
+      if (++count[num] == 1)
+        ++distinct;
+      while (distinct == totalDistinct)
+        if (--count[nums[l++]] == 0)
+          --distinct;
+      ans += l;
+    }
+
+    return ans;
+  }
+};

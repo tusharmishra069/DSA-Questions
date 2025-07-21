b9c8da0b649a56d36c7660efb68a61765929cdee
Tushar Mishra <118966111+tusharmishra069@users.noreply.github.com>
2025-07-21 20:46:43 +0530

    Time: 23 ms (76.85%), Space: 44.6 MB (39.72%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
@@ -1,23 +1,10 @@
 class Solution {
-public:
-    string makeFancyString(string s) {
-    string result;
-    int count = 1;
-
-    result += s[0];  
-
-    for (int i = 1; i < s.size(); i++) {
-        if (s[i] == s[i - 1]) {
-            count++; 
-        } else {
-            count = 1;  
-        }
-
-        if (count < 3) {
-            result += s[i];  
-        }
-    }
-
-    return result;
-    }
+ public:
+  string makeFancyString(string s) {
+    string ans;
+    for (const char c : s)
+      if (ans.length() < 2 || ans.back() != c || ans[ans.size() - 2] != c)
+        ans.push_back(c);
+    return ans;
+  }
 };

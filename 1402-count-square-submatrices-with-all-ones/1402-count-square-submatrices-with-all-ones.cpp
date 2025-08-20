60198b01e13c1b25a634cff3ec33eb7555979548
Tushar Mishra <118966111+tusharmishra069@users.noreply.github.com>
2025-08-20 21:26:55 +0530

    Time: 11 ms (28.49%), Space: 27.5 MB (92.22%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
@@ -1,25 +1,14 @@
 class Solution {
-public:
-    int countSquares(vector<vector<int>>& matrix) {
-         int m = matrix.size();
-        int n = matrix[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        int totalSquares = 0;
-
-        // Fill the dp table
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (matrix[i][j] == 1) {
-                    if (i == 0 || j == 0) {
-                        dp[i][j] = 1; // First row or first column
-                    } else {
-                        dp[i][j] = min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]}) + 1;
-                    }
-                    totalSquares += dp[i][j];
-                }
-            }
-        }
-
-        return totalSquares;
-    }
+ public:
+  int countSquares(vector<vector<int>>& matrix) {
+    for (int i = 0; i < matrix.size(); ++i)
+      for (int j = 0; j < matrix[0].size(); ++j)
+        if (matrix[i][j] == 1 && i > 0 && j > 0)
+          matrix[i][j] +=
+              min({matrix[i - 1][j - 1], matrix[i - 1][j], matrix[i][j - 1]});
+    return accumulate(matrix.begin(), matrix.end(), 0,
+                      [](int subtotal, const vector<int>& row) {
+      return subtotal + accumulate(row.begin(), row.end(), 0);
+    });
+  }
 };

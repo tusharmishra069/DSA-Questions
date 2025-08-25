bf5df331677b7e55d269f29508e56192651b545a
Tushar Mishra <118966111+tusharmishra069@users.noreply.github.com>
2025-08-25 16:14:47 +0530

    Time: 2 ms (85.6%), Space: 11.8 MB (68.55%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-    unordered_set<char> check;
-    int left = 0, right = 0, maxLength = 0;
-    
-    while (right < s.length()) {
-        if (check.find(s[right]) == check.end()) {
-            
-            check.insert(s[right]);
-            maxLength = max(maxLength, right - left + 1);
-            right++;
-        } else {
-            
-            check.erase(s[left]);
-            left++;
+        vector<int> lastIndex(256, -1);  // stores last seen index of chars
+        int maxLen = 0;
+        int left = 0;  // sliding window left pointer
+
+        for (int right = 0; right < s.size(); right++) {
+            if (lastIndex[s[right]] >= left) {
+                // character already seen, move left
+                left = lastIndex[s[right]] + 1;
+            }
+            lastIndex[s[right]] = right;  // update last seen index
+            maxLen = max(maxLen, right - left + 1);
         }
-    }
-    
-    return maxLength;
+
+        return maxLen;
     }
 };
